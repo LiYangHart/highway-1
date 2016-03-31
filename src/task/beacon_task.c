7605bf5633a7245e849059d5c12b27f735737595
Project Laptop 2 <Project email>
2016-03-31 13:22:51 -0400

    adjustment made to transmission power level for testing with case

--- src/task/beacon_task.c
+++ src/task/beacon_task.c
@@ -39,7 +39,7 @@ xbee_transmit_setup(ATDevice* xbee_transmit) {
 	//note that current settings are for transmitter, carriage return '\r' needed on end of command strings
 	//setting parameters first, will read back certain critical ones after
 
-	if (hayes_at(xbee_transmit, "ATPL1\r")							!= HAYES_OK
+	if (hayes_at(xbee_transmit, "ATPL3\r")							!= HAYES_OK
 		||hayes_res(xbee_transmit, pred_ends_with, "OK\r", 1200)		!= HAYES_OK
 		||hayes_at(xbee_transmit, "ATCE2\r")						!= HAYES_OK
 		||hayes_res(xbee_transmit, pred_ends_with, "OK\r", 1200)		!= HAYES_OK
@@ -74,7 +74,7 @@ xbee_transmit_setup(ATDevice* xbee_transmit) {
 	//after setting other parameters, configure encryption
 	if(hayes_at(xbee_transmit, "ATEE1\r")										!= HAYES_OK
 		||hayes_res(xbee_transmit, pred_ends_with, "OK\r", 1200)				!= HAYES_OK
-		||hayes_at(xbee_transmit, "ATKY0000112233445566778899AABBCCEEFF\r")		!= HAYES_OK
+		||hayes_at(xbee_transmit, "ATKY00112233445566778899AABBCCDDEEFF\r")		!= HAYES_OK
 		||hayes_res(xbee_transmit, pred_ends_with, "OK\r", 1200)				!= HAYES_OK) {
 		trace_printf("Configuring encryption failed \n");
 		return 0;
@@ -132,7 +132,7 @@ xbee_transmit_setup(ATDevice* xbee_transmit) {
 
 	//double check power and destination address fields
 	if (hayes_at(xbee_transmit, "ATPL\r")										!= HAYES_OK
-		||hayes_res(xbee_transmit, pred_ends_with, "1\r", 1200)				!= HAYES_OK ){
+		||hayes_res(xbee_transmit, pred_ends_with, "3\r", 1200)				!= HAYES_OK ){
 		trace_printf("Error in checking power level \n");
 		return 0;
 	}
